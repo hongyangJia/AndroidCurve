d8f62854e7efdcde83293415db14e76f372dc2ba
hongyang <hongyooo@icloud.com>
2017-06-27 18:16:59 +0800

    version 1.0.9

--- curve/src/main/java/curve/com/GraphView.java
+++ curve/src/main/java/curve/com/GraphView.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 public class GraphView extends LinearLayout {
 
-    private static final int INTERVAL = 5;
+    private static final int INTERVAL = 10;
     private static final int ROW = 6;
     private static final int DUFULET = 0;
     private static final int SCREEN_WIDTH_ALLOCATION = 11;
